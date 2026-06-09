4afe616462d3d22e5d67924ed466024db112543d
rqiyomova9-png <rqiyomova9@gmail.com>
2026-06-09 09:09:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from telegram.ext import (
 )
 
 # ─── KONFIGURATSIYA ────────────────────────────────────────
-BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8920261423:AAEEqttShZHe3FJ1R11bthyXQLwv30Mie3Y"
+BOT_TOKEN  = os.environ.get("BOT_TOKEN")  or "8578640575:AAFkLcw8MFN2Tra08TCvJh_K2G-J160WK-M"
 ADMIN_ID   = int(os.environ.get("ADMIN_ID") or "7812447850")
 
 DATABASE_URL      = os.environ.get("DATABASE_URL") or ""
